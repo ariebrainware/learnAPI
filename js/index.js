9c6fd6b376d6626eda1d6aa2d6d97b950241e5c5
Arie Brainware <ariebrainware@gmail.com>
2018-07-31 16:47:29 +0700

    Add README.md ,  finish level 0 - 1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,31 @@
-fetch("https://swapi.co/api/starships")
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
-    console.log(data);
-  });
+$(function () {
+  
+
+  fetch("https://swapi.co/api/")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    });
+
+  // Level 0
+  fetch("https://swapi.co/api/people/")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+    });
+
+  // Level 1
+
+  fetch("https://swapi.co/api/people/")
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      console.log(data.results);
+    });
+
+})
